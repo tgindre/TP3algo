161f2b46d6ef276d206d89c410ebdd93b8831e99
tgindre <timothee.gindre@isep.fr>
2017-03-28 19:51:32 +0200

    Mise en commentaire question 2.1

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Launcher {
 
 	public static void main(String[] args) throws IOException {
-		Graph graph1 = new Graph("graph.txt");
-		graph1.print();
+//		Graph graph1 = new Graph("graph.txt");
+//		graph1.print();
 		GraphAdjMatrix graph2 = new GraphAdjMatrix("graph.txt");
 		graph2.print();
 
-		System.out.println("Entrer le noeud dont vous voulez les voisins");
-		Scanner sc = new Scanner(System.in);
-		int v = sc.nextInt();
-		graph1.Neighbors(v);
+//		System.out.println("Entrer le noeud dont vous voulez les voisins");
+//		Scanner sc = new Scanner(System.in);
+//		int v = sc.nextInt();
+//		graph1.Neighbors(v);
 	}
 
 }
